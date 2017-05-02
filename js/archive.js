7136dbf3963b705b0ef44e5ed707060858774f66
Ramon Adan <adannup@users.noreply.github.com>
2017-05-02 13:32:29 -0500

    Curso: Condiciones IF, ELSE IF, ELSE

---
+++ js/archive.js
@@ -0,0 +1,33 @@
+var numero_uno = 30;
+var numero_dos = "30";
+
+if(numero_uno === numero_dos){
+	console.log("Entre al bloque");
+}
+
+if(numero_uno != numero_dos){
+	console.log("Devuelve falso");
+}
+
+if(numero_uno !== numero_dos){
+	console.log("Devuelve falso");
+}else if(false){
+
+}else{
+	console.log(":P");
+}
+
+/* Operadores logicos*/
+// || OR
+// && AND
+// == ===
+// != !==
+// !
+
+/*
+ Operador ==
+ 	Convierte los strings en numero primero
+
+ Operador ===
+ 	Compara antes si ambos elementos son del mismo tipo
+*/
