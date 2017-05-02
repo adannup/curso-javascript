45d273b44ce1361624cdbf5877b7fd4b34b39ff2
Ramon Adan <adannup@users.noreply.github.com>
2017-05-02 12:37:45 -0500

    Curso 2: Numeros y operaciones matematicas con funcion Math

--- js/archive.js
+++ js/archive.js
@@ -1,4 +1,29 @@
-var nombre = "Ramon Adan";
-var edad = 22;
+var edad = 25;
+edad++;
 
-document.querySelector("body").innerHTML = "Hola mundo " + nombre;
+var punto_flotante = 12.5
+
+/*
+Operadores aritmeticos
+Division = /
+Multiplicacion = *
+Suma = +
+Resta = -
+Modulo = %
+*/
+
+var resultado = edad + punto_flotante;
+var resultado_modulo = edad % 5
+
+console.log(resultado);
+console.log(resultado_modulo);
+
+//Funciones Math
+console.log('Elevar', Math.pow(2,3));
+console.log('Redondear', Math.round(0.7));
+console.log('Redondear hacia arriba', Math.ceil(0.1));
+console.log('Redondear hacia abajo', Math.floor(0.9));
+console.log('Pi', Math.PI);
+console.log('Numero aleatorio', Math.random());
+console.log('Raiz cuadrada', Math.sqrt(4));
+
